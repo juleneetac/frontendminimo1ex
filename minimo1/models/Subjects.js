a53da7a7ce6ace79438e86a04101f805e64b2a8e
Julen Garralaga <univideos8@gmail.com>
2020-03-23 14:11:47 +0100

    borrar carpeta que no era

--- minimo1/models/Subjects.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const subjects = new Schema({
-    name: String,
-    students: [{ type: mongoose.Types.ObjectId, ref: 'students' }]
-});
-module.exports = mongoose.model('subjects', subjects);
