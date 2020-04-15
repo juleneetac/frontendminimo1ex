8a022a3538e061271bda66dbd74ad896bd1b3f78
Julen Garralaga <univideos8@gmail.com>
2020-04-15 20:34:07 +0200

    quitar backend

--- minimo1backend/models/Subjects.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import mongoose = require("mongoose");
-let Schema = mongoose.Schema;
-let subjects = new Schema({
-    name: String,
-    students: [{ type: mongoose.Types.ObjectId, ref: 'students' }]
-});
-module.exports = mongoose.model('subjects', subjects);
